3f81179f140984d08614f1e0f737f25ad3c560ad
EthanNg <Ethannguyen112@gmail.com>
2024-04-10 21:09:43 -0700

    Hello World Syntax for arduino

---
+++ MiniProjects/CppPractice/arduinoSyntax.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void setup(){ // initializing the pins to use for arduino
+
+}
+
+int main(){ //for arduino it would be void loop {
+
+    cout << "Hello World";
+ 
+    return 0;
+
+}
+
