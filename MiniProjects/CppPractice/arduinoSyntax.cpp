de049ee3a677e4cf276f650daaf4f67ff3998d40
EthanNg <Ethannguyen112@gmail.com>
2024-04-11 19:49:13 -0700

    input output

--- MiniProjects/CppPractice/arduinoSyntax.cpp
+++ MiniProjects/CppPractice/arduinoSyntax.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 void setup(){ // initializing the pins to use for arduino
-
+    string s1;
+    cin >> s1;
+    cout << s1;
 }
 
 int main(){ //for arduino it would be void loop {
-
-    cout << "Hello World";
+    setup();
+    
  
     return 0;
 
