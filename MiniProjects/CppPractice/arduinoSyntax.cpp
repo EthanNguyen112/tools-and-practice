9ff41ae05d37c45513592787e008b80943685f81
EthanNg <Ethannguyen112@gmail.com>
2024-04-12 19:28:43 -0700

    created forloop function

--- MiniProjects/CppPractice/arduinoSyntax.cpp
+++ MiniProjects/CppPractice/arduinoSyntax.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int x = 0;
+int inc;
+
 void setup(){ // initializing the pins to use for arduino
-    string s1;
-    cin >> s1;
-    cout << s1;
+    //string s1;
+    cout << "Enter Input: "; 
+    cin >> inc;
+    //cout << s1 << endl;
+}
+
+void increment(){
+    
+    for (int i=0; i < inc + 1; i++){
+    //cout << i << endl;
+
+    }
+
+    x = x + inc;
+
 }
 
 int main(){ //for arduino it would be void loop {
     setup();
-    
- 
+    increment();
+
+
+    cout << "This output is " << x;
     return 0;
 
 }
