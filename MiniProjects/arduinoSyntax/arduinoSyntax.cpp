11f5300d9ef9c5ce367bb6ff048ae76452f2f6f6
EthanNg <Ethannguyen112@gmail.com>
2024-04-26 18:40:10 -0700

    Laid out rough plan

--- MiniProjects/arduinoSyntax/arduinoSyntax.cpp
+++ MiniProjects/arduinoSyntax/arduinoSyntax.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 
 //This will be a syntax using uart, camera module, iot, and more
+
+//Initialize the pins
+//Uart analoge signals
+//640 x 480 pixal camera
+
+//low power mode, off and on power mode
+
+//while on camera is uploading information into the website
+
+
+//main loop
---
+++ MiniProjects/arduinoSyntax/iotwebvideo.cpp
@@ -0,0 +1,3 @@
+//will hold the video
+//will need a password and user to access
+//allow to be viewed on phone and on desktop
